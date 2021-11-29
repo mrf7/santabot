8031c29b1bbd9b7aabdf2f4958141687ef6717a4
Michael Friend <michaelf@touchlab.co>
2021-11-28 20:26:41 -0500

    trying extensions,m it crashes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,6 +16,70 @@ import dev.kord.core.entity.Message
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
 
-val TEST_SERVER= Snowflake(env("TEST_GUILD"))
+val TEST_SERVER = Snowflake(env("TEST_GUILD"))
 val TOKEN = env("TOKEN")
-suspend fun main(args: Array<String>) {}
+suspend fun main(args: Array<String>) {
+    val bot = ExtensibleBot(TOKEN) {
+        chatCommands { enabled = true }
+        applicationCommands { enabled = true }
+        extensions { add(::SantaExtension) }
+    }
+    bot.start()
+}
+
+class SantaExtension : Extension() {
+    override val name: String = "santa"
+
+    override suspend fun setup() {
+        publicSlashCommand {
+            name = "bingbong"
+            description = "slippy slappy"
+            guild(TEST_SERVER)
+            action {
+                respond { content = "slippy slappy" }
+            }
+
+        }
+        publicSlashCommand(::SlapSlashArgs) {  // Public slash commands have public responses
+            name = "slap"
+            description = "Ask the bot to slap another user"
+
+            // Use guild commands for testing, global ones take up to an hour to update
+            guild(TEST_SERVER)
+
+            action {
+                val kord = this@SantaExtension.kord
+
+                val realTarget = if (arguments.target.id == kord.selfId) {
+                    member
+                } else {
+                    arguments.target
+                }
+
+                respond {
+                    content = "*slaps ${realTarget?.mention} with their ${arguments.weapon}*"
+                }
+            }
+        }
+        chatCommand {
+            name = "chatCommand"
+            description = "test chat"
+            action {
+                message.respond("bing")
+            }
+        }
+    }
+
+}
+
+class SlapSlashArgs : Arguments() {
+    val target by user("target", description = "Person you want to slap")
+
+    // Coalesced strings are not currently supported by slash commands
+    val weapon by defaultingString(
+        "weapon",
+
+        defaultValue = "large, smelly trout",
+        description = "What you want to slap with"
+    )
+}
