dc1e7c7132c3994609fe2046176d664586adc1e9
Michael Friend <michaelf@touchlab.co>
2021-11-28 20:25:35 -0500

    added env

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,21 @@
 import com.kotlindiscord.kord.extensions.ExtensibleBot
+import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.converters.impl.defaultingString
+import com.kotlindiscord.kord.extensions.commands.converters.impl.user
+import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.extensions.chatCommand
+import com.kotlindiscord.kord.extensions.extensions.publicMessageCommand
+import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
+import com.kotlindiscord.kord.extensions.types.respond
+import com.kotlindiscord.kord.extensions.utils.env
+import com.kotlindiscord.kord.extensions.utils.respond
+import dev.kord.cache.api.data.description
+import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
 import dev.kord.core.entity.Message
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
 
-suspend fun main(args: Array<String>) {
-    val bot = ExtensibleBot("OTEzOTI1NTczODc1NzQ0ODE4.YaFlhA.5lF1_gXmKO8ypYlm2K0SaLtRH-E") {
-        applicationCommands {
-            slashCommandCheck {
-
-            }
-        }
-    }
-    bot.start()
-}
+val TEST_SERVER= Snowflake(env("TEST_GUILD"))
+val TOKEN = env("TOKEN")
+suspend fun main(args: Array<String>) {}
